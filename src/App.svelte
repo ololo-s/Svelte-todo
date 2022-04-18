7d872d7f7f033c0b4a6bb1cfe1aaf975c461112b
Olha Solovei <o.keksovna@gmail.com>
2022-04-18 10:48:23 +0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,62 @@
+<script>
+    import Todo from "./Todo.svelte";
+</script>
+
+<body>
+<div class="container">
+  <header>
+    <h1>TODO</h1>
+  </header>
+  <main>
+    <Todo/>
+  </main>
+  <footer>
+    <p>That's all</p>
+  </footer>
+</div>
+</body>
+
+<style>
+    body {
+        font-family: "Calibri Light";
+    }
+
+    main {
+        text-align: center;
+        padding: 1em;
+        min-height: 100vh;
+    }
+
+    .container {
+        max-width: 840px;
+        margin: 0 auto;
+        background-color: #f4f4f4;
+    }
+
+    header {
+        padding: 5%;
+        background-image: linear-gradient(to bottom right, #57ddff, #c058f3);
+    }
+
+    h1 {
+        color: #fff;
+        text-transform: uppercase;
+        font-size: 3em;
+        letter-spacing: 5px;
+        line-height: 150%;
+        font-weight: 600;
+        text-shadow: #999999 1px 1px;
+    }
+
+    p {
+        font-size: 14px;
+        text-align: center;
+        font-weight: 600;
+    }
+
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
+</style>
---
+++ src/Todo.svelte
@@ -0,0 +1,102 @@
+<script>
+    let todos = JSON.parse(localStorage.getItem('todos')) || [
+        {completed: false, text: ''},
+    ];
+
+    function addTodo() {
+        todos = todos.concat({completed: false, text: ''});
+    }
+
+    function deleteCompleted() {
+        todos = todos.filter(item => !item.completed);
+    }
+
+    $: incomplete = todos.filter(item => !item.completed).length - 1;
+    $: localStorage.setItem('todos', JSON.stringify(todos));
+</script>
+
+<!--Ctrl+Alt+L formatting code-->
+
+<div class="form">
+  <form>
+    {#each todos as todo}
+      <div>
+        <input type="checkbox" bind:checked={todo.completed} disabled={todo.text===''}>
+        <input type="text" placeholder="Add a new todo" bind:value={todo.text} disabled={todo.completed}>
+      </div>
+    {/each}
+    <div class="btn">
+      <button on:click|preventDefault={addTodo} disabled={!todos[todos.length - 1].text}>Add</button>
+      <button on:click|preventDefault={deleteCompleted} disabled={todos.length < 2}>Delete completed</button>
+    </div>
+  </form>
+  {#if incomplete > 0}
+    <p>Left {incomplete} todo(s)</p>
+    {:else}
+    <p>Done!</p>
+  {/if}
+</div>
+
+<style>
+    form {
+        margin-top: -54px;
+        max-width: 840px;
+        display: flex;
+        flex-direction: column;
+    }
+
+    input {
+        padding: 8px;
+    }
+    input[type=checkbox] {
+        opacity: 100%;
+        border: 2px solid #ddd;
+        display: inline-block;
+        vertical-align: middle;
+        width: 18px;
+        height: 18px;
+    }
+
+    input[type=text] {
+        display: inline-block;
+        width: 70%;
+        border-radius: 5px;
+        letter-spacing: 1px;
+        padding-left: 10px;
+    }
+
+    button {
+        display: block;
+        background-color: #fff;
+        min-width: 25%;
+        border-radius: 5px;
+        margin: 0 10px;
+        box-shadow: #999999 1px 1px;
+    }
+
+    button:disabled {
+        color: #6e6e6e;
+        letter-spacing: 1px;
+    }
+
+    .btn {
+        display: flex;
+        justify-content: center;
+    }
+
+    button:first-child {
+        background-image: linear-gradient(to bottom right, #57ddff, #c058f3);
+        opacity: 60%;
+        color: #000;
+    }
+
+    p {
+        display: inline-block;
+        padding: 5px 110px;
+        background-color: #c0bfbf;
+        color: #fff;
+        letter-spacing: 1px;
+        margin-top: 40px;
+        font-size: 18px;
+    }
+</style>
