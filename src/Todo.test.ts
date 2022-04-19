0d5abda6ed55d7c0f387298c0120312f33dd78d9
Olha Solovei <o.keksovna@gmail.com>
2022-04-19 13:07:05 +0300

    add test 'loading from localStorage'

--- src/Todo.test.ts
+++ src/Todo.test.ts
@@ -1,4 +1,6 @@
-import Todo, {save} from "./Todo";
+import Todo, {save, load} from "./Todo";
+import {stringify} from "vitest";
+
 
 it('is empty by default', () => {
     const todo = new Todo()
@@ -10,6 +12,14 @@ it('saves to localStorage', () => {
     const todo = new Todo('Wash the dishes')
     save([todo])
     expect(localStorage.getItem('todos')).eq(`[{"text":"Wash the dishes","completed":false}]`)
+})
+
+it('loading from localStorage', () => {
+    localStorage.setItem('todos', `[{"text":"Wash the dishes","completed":true}]`)
+    const todos = load()
+    expect(todos.length).eq(1)
+    expect(todos[0].text).eq('Wash the dishes')
+    expect(todos[0].completed).eq(true)
 })
 
 it('math works', () => {
