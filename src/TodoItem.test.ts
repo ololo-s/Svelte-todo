fa8083d4af10f1656c1a0253d1c10feb12436973
Olha Solovei <o.keksovna@gmail.com>
2022-04-20 10:27:32 +0300

    add tests 'renders empty item', 'renders completed item'

---
+++ src/TodoItem.test.ts
@@ -0,0 +1,20 @@
+import TodoItem from "./TodoItem.svelte"
+import {cleanup, fireEvent, getByDisplayValue, queryByAttribute, render, screen} from '@testing-library/svelte';
+import Todo from "./Todo";
+
+it('renders empty item', ()=> {
+    const {container} = render(TodoItem, {todo: new Todo()})
+    const text = container.querySelector('input[type=text]') as HTMLInputElement
+    expect(text.value).eq('')
+    const checkbox = container.querySelector('input[type=checkbox]') as HTMLInputElement
+    expect(checkbox.checked).eq(false)
+})
+
+it('renders completed item', ()=> {
+    const {container} = render(TodoItem, {todo: new Todo('Feed a dog', true)})
+    const text = container.querySelector('input[type=text]') as HTMLInputElement
+    expect(text.value).eq('Feed a dog')
+    const checkbox = container.querySelector('input[type=checkbox]') as HTMLInputElement
+    expect(checkbox.checked).eq(true)
+})
+
